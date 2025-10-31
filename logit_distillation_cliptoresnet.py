1d968e7bc20c55471a1986a1d850c3fe4d72570d
Avinash Gyavali <avinashgyavali190@gmail.com>
2025-10-31 12:01:32 -0400

    some problem in distillation

--- logit_distillation_cliptoresnet.py
+++ logit_distillation_cliptoresnet.py
@@ -22,11 +22,11 @@ import numpy as np
 TRAIN_SUBSET_RATIO = 0.15
 # For cluster server
 
-TRAIN_DIR = '/home/c3-0/datasets/ImageNet/train'
-VAL_DIR = '/home/c3-0/datasets/ImageNet/validation'
+# TRAIN_DIR = '/home/c3-0/datasets/ImageNet/train'
+# VAL_DIR = '/home/c3-0/datasets/ImageNet/validation'
 
-# TRAIN_DIR = '~/data/datasets/imagenet/train'
-# VAL_DIR = '~/data/datasets/imagenet/val'
+TRAIN_DIR = '~/data/datasets/imagenet/train'
+VAL_DIR = '~/data/datasets/imagenet/val'
 VAL_SUBSET_SIZE = 5000
 BATCH_SIZE = 32
 LEARNING_RATE = 1e-4
