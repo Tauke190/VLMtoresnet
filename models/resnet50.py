7f0be1a9f8b5ff39aecd560cb6f1722e62177eb5
Avinash Gyavali <avinashgyavali190@gmail.com>
2025-11-05 18:05:58 -0500

    hello world

--- models/resnet50.py
+++ models/resnet50.py
@@ -4,7 +4,11 @@ import torchvision.models as models
 class ResNet50(nn.Module):
     def __init__(self, num_classes):
         super(ResNet50, self).__init__()
-        self.model = models.resnet50(weights=None)
+        # Handle both old (pretrained=...) and new (weights=...) APIs
+        try:
+            self.model = models.resnet50(weights=None)
+        except TypeError:
+            self.model = models.resnet50(pretrained=False)
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
     def forward(self, x):
