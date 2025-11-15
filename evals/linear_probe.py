a67346f9c0b343a00c1dbb46479c2abc9a8471d1
Avinash Gyavali <avinashgyavali190@gmail.com>
2025-11-14 23:25:45 -0500

    linear probing still not working

--- evals/linear_probe.py
+++ evals/linear_probe.py
@@ -29,7 +29,7 @@ args = parser.parse_args()
 
 DATA_ROOT = DATASET_PATHS[args.dataset]
 
-BATCH_SIZE = 32
+BATCH_SIZE = 256
 NUM_WORKERS = 8
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
