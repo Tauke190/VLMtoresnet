48489525052141d96272293e030138b76056be11
Avinash Gyavali <avinashgyavali190@gmail.com>
2025-11-21 16:14:25 -0500

    resnet 50 non local attention maps

--- test/resnet50_non_local.py
+++ test/resnet50_non_local.py
@@ -112,7 +112,7 @@ def visualize_attention(img, attn_map, block_name="block"):
 # Main
 # -------------------------------
 if __name__ == "__main__":
-    img_path = "your_image.jpg"  # replace with your image
+    img_path = "dog.jpg"  # replace with your image
     img, img_tensor = load_image(img_path)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
