17a5c68c139fde154f423b6b09f945e85030379d
Avinash Gyavali <avinashgyavali190@gmail.com>
2025-11-28 18:42:11 -0500

    added a single traininig script to select modes

---
+++ utils/__init__.py
@@ -0,0 +1,11 @@
+from .utils import (
+    zeroshot_classifier,
+    get_teacher_features,
+    get_student_features,
+    imagenet_aligned_classnames,
+    imagefolder_human_names,
+    read_txt,
+    save_checkpoint,
+    compute_flops,
+    plot_and_save_losses,
+)
