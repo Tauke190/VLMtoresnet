a693cc777c04972bef2a7ba81e5e3b8a0af9e44d
Avinash Gyavali <avinashgyavali190@gmail.com>
2025-12-09 16:42:26 -0500

    added a test file to check model ma36 dimension

---
+++ ml-fastvit-main/test.py
@@ -0,0 +1,27 @@
+import torch
+from timm.models import create_model
+import clip
+
+device = "cuda"
+
+# FastViT
+model = create_model("fastvit_sa36", pretrained=False).to(device)
+model.eval()
+
+x = torch.randn(2, 3, 256, 256, device=device)
+with torch.no_grad():
+    if hasattr(model, "forward_features"):
+        feats = model.forward_features(x)
+    else:
+        feats = model(x)
+    if isinstance(feats, (tuple, list)):
+        feats = feats[0]
+    feats = feats.view(feats.size(0), -1)
+print("FastViT feat dim:", feats.shape[-1])
+
+# CLIP
+clip_model, _ = clip.load("ViT-L/14", device=device, jit=False)
+with torch.no_grad():
+    tokens = clip.tokenize(["a dog"]).to(device)
+    txt = clip_model.encode_text(tokens)
+print("CLIP text dim:", txt.shape[-1])
