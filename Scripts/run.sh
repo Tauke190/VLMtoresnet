f0037e4df26e667992bf3a224c46c120ebaa668d
Tauke190 <avinashgyavali190@gmail.com>
2026-01-25 17:04:52 -0500

    training code ready for lrtoken and adapter

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -6,16 +6,16 @@ conda activate fastvit
 
 
 ############## Training 
-WT=logs/fastvit_sa36.pth.tar
+WT=Weights/fastvit_sa36.pth.tar
 IMAGENET_PATH=/mnt/SSD2/ImageNet1k/
 EXP=CLIPtoResNet
 VAL_SET="food101"
 VAL_PATH=/mnt/SSD2/food-101
 OUTPUT=./checkpoints
+NUM_GPU=2
 
 
-
-
+#[fastvit_sa36_adapter,fastvit_sa36_lrtokens]
 MODEL=fastvit_sa36_adapter
 CUDA_VISIBLE_DEVICES=0,2 python -m torch.distributed.launch --nproc_per_node=$NUM_GPU train_baseline.py \
     $IMAGENET_PATH --model $MODEL --val-set $VAL_SET --validation-data-dir $VAL_PATH \
@@ -23,12 +23,3 @@ CUDA_VISIBLE_DEVICES=0,2 python -m torch.distributed.launch --nproc_per_node=$NU
     --freeze-backbone --native-amp --workers 12 --clip-loss-weight 1 \
     -b 32 --lr 1e-3 --drop-path 0.35 --mixup 0 --cutmix 0 --epochs 50 --input-size 3 224 224 
     
-
-
-# --checkpoint-name fastvitsa36_projector_lrtokens.pth.tar \       # 
-# --log-wandb --experiment CLIPtoResNet \
-# --debug
-
-
-
---
