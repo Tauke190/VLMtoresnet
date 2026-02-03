4f68b50dd4b8b68b372da8199998d075598f7c75
Tauke190 <avinashgyavali190@gmail.com>
2026-02-03 17:20:17 -0500

    training again

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,13 +13,13 @@ VAL_SET="food101"
 VAL_PATH=/mnt/SSD2/food-101
 OUTPUT=./checkpoints
 NUM_GPU=2
-MODEL=fastvit_sa36_lrtokens
+MODEL=fastvit_sa36_adapter
 
 # Model = [fastvit_sa36, fastvit_sa36_adapter, fastvit_sa36_lrtokens]
 # Training methods = [default, baseline, distillation]
 
 CUDA_VISIBLE_DEVICES=0,2 python -m torch.distributed.launch --nproc_per_node=$NUM_GPU train_baseline.py \
     $IMAGENET_PATH --model $MODEL --val-set $VAL_SET --validation-data-dir $VAL_PATH \
-    --method distillation --validation-eval-interval 2000 --initial-checkpoint $WT --output $OUTPUT --experiment $EXP \
+    --method baseline --validation-eval-interval 2000 --initial-checkpoint $WT --output $OUTPUT --experiment $EXP \
     --freeze-backbone --native-amp --workers 12 \
     -b 32 --lr 1e-3 --drop-path 0.35 --mixup 0 --cutmix 0 --epochs 50 --input-size 3 224 224
