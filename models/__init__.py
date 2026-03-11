3151a84f6a45a16dae08a5c994d36aa57741b9b9
Arnav Chauhan <arnavch246@gmail.com>
2026-03-11 18:01:02 +0530

    added missing imports

--- models/__init__.py
+++ models/__init__.py
@@ -12,8 +12,19 @@ from .fastvit import (
     fastvit_ma36,
 )
 
-from .fastvit_proposed import IMPORT_NONE, fastvit_sa36_projector
+from .fastvit_proposed import (
+    IMPORT_NONE,
+    fastvit_sa36_projector,
+    fastvit_sa36_adapter,
+    fastvit_sa36_lrtokens,
+    fastvit_sa36_lora,
+    fastvit_sa36_lora_pp,
+)
 
-from .fastvit_arnav import fastvit_sa36_nonlocal  # register nonlocal model with timm
+from .fastvit_arnav import (
+    fastvit_sa36_nonlocal,
+    fastvit_sa36_mhsa,
+    fastvit_sa36_neckprobe,
+)
 
 VANILLA_MODELS = ['fastvit_t8', 'fastvit_t12', 'fastvit_s12', 'fastvit_sa12', 'fastvit_sa24', 'fastvit_sa36', 'fastvit_ma36']
